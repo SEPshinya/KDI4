78c3748638be409d5e3a9feae7098d54c02955a4
harashinya1192 <harashinya1192@host.docker.internal>
2020-10-06 10:03:15 +0900

    完

--- demo3/src/main/java/com/example/demo/UserUpdateRequest.java
+++ demo3/src/main/java/com/example/demo/UserUpdateRequest.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,7 +11,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper=false)
 public class UserUpdateRequest extends UserRequest implements Serializable {
 
-@NotNull
+
 private Long id;
 
 }
